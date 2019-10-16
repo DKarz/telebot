29a74f3d7187f344435e3d2a23be54959f89a26c
DKarz <49778314+DKarz@users.noreply.github.com>
2019-10-16 10:40:46 +0300

    Update variables.py

--- variables.py
+++ variables.py
@@ -5,3 +5,4 @@ subjects = ["calculus"]
 smartmode = False
 flag = False
 inv_req = ""
+discript = '''The following telegram-bot is created to make students' lives a bit easier. The main aim of it is collecting some data at one place, data which is spread among the internet and emails. At the current moment the lack of the bot's commands does not give an opportunity to use it frequently. However, the mastering of the program is still in process, so some improvements are about to be achieved. Moreover, any suggestions are welcome, thus do not hesitate to inform the "author" about it.'''
